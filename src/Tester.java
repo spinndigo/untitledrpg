c37ee6f0e4c72ffe9576901faa576ec11591a30a
tlprotzman <tlprotzman+github@gmail.com>
2016-06-18 13:00:01 -0400

    create tester class to test character class functionallity

---
+++ src/Tester.java
@@ -0,0 +1,14 @@
+/*
+Tristan Protzman
+
+Tester class*/
+
+public class Tester{
+
+	public static void main(String[] args){
+
+		
+		Character james = new Character("James", 5, 4, 2, 6, 3, 100); 
+
+	}
+}
