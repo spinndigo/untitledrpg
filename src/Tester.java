2abdfe226b63a89198a48454209bbb4f851daa2c
tlprotzman <tlprotzman+github@gmail.com>
2016-06-18 13:29:08 -0400

    added comments

--- src/Tester.java
+++ src/Tester.java
@@ -9,11 +9,11 @@ public class Tester{
 
 		
 		Character james = new Character("James", 5, 4, 2, 6, 3, 100); 
-		System.out.println(james.getHealth());
-		james.changeHealth(-30);
-		System.out.println(james.getHealth());
-		james.changeHealth(500);
-		System.out.println(james.getHealth());
+		System.out.println(james.getHealth());		//gets initial health
+		james.changeHealth(-30);					//decreces health by 30
+		System.out.println(james.getHealth());		
+		james.changeHealth(500);					//increases health by more than maximum health
+		System.out.println(james.getHealth());		//makes sure health did not go above maximum
 
 	}
 }
