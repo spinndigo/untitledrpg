12e4d11222924cbacbcce01415ba3175e29c1b8c
tlprotzman <tlprotzman+github@gmail.com>
2016-06-21 21:44:45 -0400

    init consumable class

---
+++ src/Consumable.java
@@ -0,0 +1,35 @@
+/*
+Tristan Protzman
+
+Class for all consumable items in the game, ie scrolls and potions
+*/
+
+/*
+TODO
+
+*/
+
+public class Consumable extends Item{
+
+	private int duration;
+	private double modStrength;
+	private double modDefense;
+	private double modAgility;
+	private boolean useOnEnemy;
+	private boolean useOutOfBattle;
+
+
+	public Consumable(String name, String description, int cost, int duration, double modStrength, double modDefense, double modAgility, boolean useOnEnemy, boolean useOutOfBattle){
+		this.name = name;
+		this.description = description;
+		this.cost = cost;
+		this.duration = duration;
+		this.modStrength = modStrength;
+		this.modDefense = modDefense;
+		this.modAgility = modAgility;
+		this.useOnEnemy = useOnEnemy;
+		this.useOutOfBattle = useOutOfBattle;
+	}
+
+
+}
