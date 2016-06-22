48b22e97be3e1ef835464c766e1aba28e4449e65
tlprotzman <tlprotzman+github@gmail.com>
2016-06-21 21:28:47 -0400

    generalize class for all objects

--- src/Item.java
+++ src/Item.java
@@ -20,35 +20,20 @@ public class Item{
 	//item statistics
 	private String name;
 	private String description;
-	private boolean consumable;
 	private int cost;
-	private double modStrength;
-	private double modDefense;
-	private double modAgility;
 
 	//contsructors
-	public Item(String name, String description, boolean consumable, int cost, double modStrength, double modDefense, double modAgility){
-		this.name = name;
-		this.description = description;
-		this.consumable = consumable;
-		this.cost = cost;
-		setModifiers(modStrength, modDefense, modAgility);
-	}
 
 	public Item(String name){
 		this.name = name;
 		this.description = "An useless item";
-		this.consumable = false;
 		this.cost = 0;
-		setModifiers(1, 1, 1);
 	}
 
-	public Item(String name, int cost){
+	public Item(String name, String description, int cost){
 		this.name = name;
-		this.description = "A sellable item";
-		this.consumable = false;
+		this.description = description;
 		this.cost = cost;
-		setModifiers(1, 1, 1);
 	}
 
 
@@ -63,32 +48,9 @@ public class Item{
 		return description;
 	}
 
-	public boolean isConsumable(){
-		return consumable;
-	}
-
 	public int getCost(){
 		return cost;
 	}
 
-	public double getStrength(){
-		return modStrength;
-	}
-
-	public double getDefense(){
-		return modDefense;
-	}
-
-	public double getAgility(){
-		return modAgility;
-	}
-
-	
 	//methods
-	private void setModifiers(double strength, double defense, double agility){
-		this.modStrength = strength;
-		this.modDefense = defense;
-		this.modAgility = agility;
-	}
-
 }
